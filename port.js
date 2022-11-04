0ee902d17b2f44ccf4df0671fdaf37660fbddd1d
nuston_pay <43966559+nuston@users.noreply.github.com>
2022-11-04 21:15:34 +0100

    Create port.js

---
+++ port.js
@@ -0,0 +1,27 @@
+let toggle = document.querySelector(".toggle");
+let topbar = document.querySelector(".topbar");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+let themeSwitch = document.querySelector(".themeSwitch");
+let body = document.querySelector("body"); 
+
+toggle.onclick = function () {
+  toggle.classList.toggle('active');
+  topbar.classList.toggle('active');
+  navigation.classList.toggle('active');
+  main.classList.toggle('active');
+}
+
+themeSwitch.onclick = function(){
+  body.classList.toggle('dark');
+}
+
+function toggleMenu(){
+
+  let navigation = document.querySelector(".navigation");
+  let main = document.querySelector(".main");
+
+  navigation.classList.remove('active');
+  main.classList.remove('active');
+  
+}
